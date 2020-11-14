6a2cd0ee8dc3a8afa28de4a0122367a129865a18
Martin-Surface <mxz0801@gmail.com>
2020-11-14 11:03:19 -0800

    setup database

--- TinNews/app/src/main/java/com/example/tinnews/repository/NewsRepository.java
+++ TinNews/app/src/main/java/com/example/tinnews/repository/NewsRepository.java
@@ -1,10 +1,14 @@
 package com.example.tinnews.repository;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.tinnews.TinNewsApplication;
+import com.example.tinnews.database.TinNewsDatabase;
+import com.example.tinnews.model.Article;
 import com.example.tinnews.model.NewsResponse;
 import com.example.tinnews.newwork.NewsApi;
 import com.example.tinnews.newwork.RetrofitClient;
@@ -15,11 +19,47 @@ import retrofit2.Response;
 
 public class NewsRepository {
     private final NewsApi newsApi;
+    private final TinNewsDatabase database;
 
 
     public NewsRepository(Context context) {
         newsApi = RetrofitClient.newInstance(context).create(NewsApi.class);
+        database = ((TinNewsApplication) context.getApplicationContext()).getDatabase();
     }
+
+    private static class FavoriteAsyncTask extends AsyncTask<Article, Void, Boolean> {
+        private final TinNewsDatabase database;
+        private final MutableLiveData<Boolean> liveData;
+
+        private FavoriteAsyncTask(TinNewsDatabase database, MutableLiveData<Boolean> liveData){
+            this.database = database;
+            this.liveData = liveData;
+        }
+
+        @Override
+        protected Boolean doInBackground(Article... articles) {
+            Article article = articles[0];
+            try {
+                database.articleDao().saveArticle(article);
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            liveData.setValue(success);
+        }
+}
+
+    public LiveData<Boolean> favoriteArticle(Article article) {
+        MutableLiveData<Boolean> resultLiveData = new MutableLiveData<>();
+        new FavoriteAsyncTask(database, resultLiveData).execute(article);
+        return resultLiveData;
+    }
+
+
     public LiveData<NewsResponse> getTopHeadlines(String country) {
         MutableLiveData<NewsResponse> topHeadlines = new MutableLiveData<>();
         newsApi.getTopHeadlines(country)
--- TinNews/app/src/main/java/com/example/tinnews/ui/home/HomeFragment.java
+++ TinNews/app/src/main/java/com/example/tinnews/ui/home/HomeFragment.java
@@ -112,6 +112,9 @@ public class HomeFragment extends Fragment implements CardStackListener {
         } else if (direction == Direction.Right) {
             Log.d("CardStackView", "Liked "  + layoutManager.getTopPosition());
         }
+
+        Article article = articles.get(layoutManager.getTopPosition()-1);
+        viewModel.setFavoriteArticleInput(article);
     }
 
 
